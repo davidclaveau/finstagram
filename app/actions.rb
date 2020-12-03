c4340a53b2032be55092d11d6081fde56e668022
davidclaveau <daveclaveau@gmail.com>
2020-12-03 04:28:11 +0000

    add routes for get, post 'signup'

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,29 @@
 get '/' do
-    "Hello World!"
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
+
+post '/signup' do
+    
+    # grab user input values from params
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+
+    
+  # instantiate a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  # if user validations pass and user is saved
+  if @user.save
+    "User #{username} saved"
+  else
+    erb(:signup)
+  end
 end
